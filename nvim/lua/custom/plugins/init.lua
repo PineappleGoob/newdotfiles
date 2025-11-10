ef66015a2d07816587f3a00aa13b4fd43f1d9570
PinePlayz <88686590+PineappleGoob@users.noreply.github.com>
2025-11-09 20:21:25 -0500

    Add files via upload

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,14 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {{
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+        -- You can choose one of the following pickers
+        'nvim-telescope/telescope.nvim',
+        'ibhagwan/fzf-lua',
+        'echasnovski/mini.pick',
+		'folke/snacks.nvim',
+    },
+}}
